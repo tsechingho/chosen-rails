c7447863cd751ffde35ff37f1c3e04fbd614d175
Tse-Ching Ho <tsechingho@gmail.com>
2013-06-04 10:11:05 +0800

    bump version

--- lib/chosen-rails.rb
+++ lib/chosen-rails.rb
@@ -2,13 +2,14 @@ require 'chosen-rails/version'
 
 module Chosen
   module Rails
-    case ::Rails.version.to_s
-    when /^4/
-      require 'chosen-rails/engine'
-    when /^3\.[12]/
-      require 'chosen-rails/engine3'
-    when /^3\.[0]/
-      require 'chosen-rails/railtie'
-    end
   end
+end
+
+case ::Rails.version.to_s
+when /^4/
+  require 'chosen-rails/engine'
+when /^3\.[12]/
+  require 'chosen-rails/engine3'
+when /^3\.[0]/
+  require 'chosen-rails/railtie'
 end
