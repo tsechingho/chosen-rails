369e310daeabfa59623a3a19215ccdba574b3b50
Rob Kilby <rob.kilby@smithelectric.com>
2015-09-03 10:55:17 +0100

    RSpec feature helpers

---
+++ lib/chosen-rails/rspec.rb
@@ -0,0 +1,103 @@
+module Chosen
+  module Rspec
+    module FeatureHelpers
+      def chosen_select(value, *args)
+        options = args.extract_options!
+
+        fail ArgumentError, 'Required argument from: not set' unless options.has_key?(:from)
+
+        from = options.delete(:from)
+
+        begin
+          input = chosen_find_container(from, options)
+        rescue Capybara::ElementNotFound
+          input = chosen_find_input(from, options)
+        end
+
+        args.unshift(value).uniq.each { |item| chosen_select!(input, item) }
+      end
+
+      def chosen_unselect(value, *args)
+        options = args.extract_options!
+
+        fail ArgumentError, 'Required argument from: not set' unless options.has_key?(:from)
+
+        from = options.delete(:from)
+
+        begin
+          input = chosen_find_container(from, options)
+        rescue Capybara::ElementNotFound
+          input = chosen_find_input(from, options)
+        end
+
+        args.unshift(value).uniq.each do |item|
+          if chosen_multiselect?(input)
+            chosen_unselect_multiple!(input, item)
+          else
+            chosen_unselect_single!(input)
+          end
+        end
+      end
+
+      private
+
+      def chosen_find_container(from, options)
+        id = from
+        id = "##{id}" unless from.start_with?('#')
+        id = "#{id}_chosen" unless from.end_with?('_chosen')
+
+        find(:div, id, options)
+      rescue Capybara::ElementNotFound
+        label = find('label', { text: from }.merge(options))
+
+        find(:div, "##{label[:for]}_chosen", options)
+      end
+
+      def chosen_find_input(from, options)
+        from = "##{from}" unless from.start_with?('#')
+
+        find(:select, from, options)
+      end
+
+      def chosen_multiselect?(input)
+        input.first('.chosen-container-multi').present?
+      end
+
+      def chosen_select!(input, item)
+        if input.tag_name == 'select'
+          input.find(:option, item).select_option
+        else
+          input.click
+
+          within '.chosen-drop .chosen-results' do
+            result = find('.active-result', text: item)
+
+            result.click if result.visible?
+          end
+        end
+      end
+
+      def chosen_unselect_single!(input)
+        if input.tag_name == 'select'
+          input.find(:option, { selected: :selected }).unselect_option
+        else
+          input.first('.search-choice-close').click
+        end
+      end
+
+      def chosen_unselect_multiple!(input, item)
+        if input.tag_name == 'select'
+          input.find(:option, item).unselect_option
+        else
+          input.first('.search-choice', text: item)
+            .first('.search-choice-close')
+            .click
+        end
+      end
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include Chosen::Rspec::FeatureHelpers, type: :feature
+end
