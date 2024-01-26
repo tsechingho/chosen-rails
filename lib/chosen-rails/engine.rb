adf118757992895dd1c16742c0d1219d8c5c5c22
lfarrell <lfarrell@email.unc.edu>
2024-01-26 13:54:56 -0500

    Update how assets are configured

--- lib/chosen-rails/engine.rb
+++ lib/chosen-rails/engine.rb
@@ -1,9 +1,10 @@
 module Chosen
   module Rails
     class Engine < ::Rails::Engine
-      config.assets.precompile += %w(
-        chosen-sprite*.png
-      )
+
+      initializer "chosen-rails.assets.precompile" do |app|
+        app.config.assets.precompile += %w( chosen-sprite*.png )
+      end
 
       rake_tasks do
         load 'chosen-rails/tasks.rake'
